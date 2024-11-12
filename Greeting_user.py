d6ce51de97b45de15acdf809193877ffda4e03ea
EndrexAkoto <martinakoto3@gmail.com>
2024-11-12 18:09:29 +0300

    greetings code for name and color

---
+++ Greeting_user.py
@@ -0,0 +1,11 @@
+# Personalized Greeting App
+print("Welcome to the Personalized Greeting App!")
+
+# Ask for the user's name
+name = input("What's your name? ")
+
+# Ask for the user's favorite color
+favorite_color = input("What's your favorite color? ")
+
+# Print a personalized greeting
+print(f"Hello, {name}! Your favorite color, {favorite_color}, is awesome!")
